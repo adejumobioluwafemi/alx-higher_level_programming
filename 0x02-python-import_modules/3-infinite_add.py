9277b0062c47210388664650010a433eb685ef08
adejumobioluwafemi <olufemiphil7@gmail.com>
2022-06-08 23:50:54 +0100

    python import module tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+from sys import argv
 
-if __name__ == "__main__":
-    import sys
+i, res = 1, 0
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
-
+if __name__ == '__main__':
+    while i < len(argv):
+        res += int(argv[i])
+        i += 1
+    print(res)
