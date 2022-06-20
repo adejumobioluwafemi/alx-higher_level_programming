755f33b7ac328b90333e1681372ce4b37f0b1d3c
adejumobioluwafemi <olufemiphil7@gmail.com>
2022-06-20 23:35:08 +0100

    updated exception tasks

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
+        return True
+    except BaseException:
         return False
-    return True
